259ca0f666cfd5cd7498c9be3bf3f6961d3162c1
Katsu <evergonuaa@gmail.com>
2019-02-21 18:24:23 +0800

    add NewConfig

--- config/config.go
+++ config/config.go
@@ -2,40 +2,77 @@ package config
 
 import (
 	"bytes"
+	"errors"
 	"io"
+	"os"
+	"strings"
 
 	"gopkg.in/yaml.v2"
 
 	"github.com/pelletier/go-toml"
 )
 
-type ConfigFile struct {
+//Tiny describes the struct for gDNS config
+type Tiny struct {
 	ConfigFileName string
 	ConfigType     string
 	ParseResult    map[string]interface{}
 	ConfigErr      error
 }
 
-//
-func (c *ConfigFile) ReadConfig(cfgio io.Reader) {
+//NewConfig will initialize the config
+func NewConfig(cfgfile string) *Tiny {
+	t := &Tiny{
+		ConfigFileName: cfgfile,
+		ConfigType:     "",
+		ParseResult:    make(map[string]interface{}),
+		ConfigErr:      nil,
+	}
+
+	//open config file failed
+	if _, err := os.Open(cfgfile); err != nil {
+		t.ConfigErr = err
+		return t
+	}
+
+	//determine the config type by suffix of file name, toml or yaml
+	for _, cfgtype := range []string{"toml", "tml", "yaml", "yml"} {
+		if strings.HasSuffix(cfgfile, cfgtype) {
+			t.ConfigType = cfgtype
+			break
+		}
+	}
+
+	if t.ConfigType == "" {
+		t.ConfigErr = errors.New("not supported config file")
+	}
+
+	return t
+}
+
+//ReadConfig will read from config file and write it into ParseResult
+func (t *Tiny) ReadConfig(cfgio io.Reader) {
 	buf := new(bytes.Buffer)
 	if _, err := buf.ReadFrom(cfgio); err != nil && err != io.EOF {
-		c.ConfigErr = err
+		t.ConfigErr = err
 		return
 	}
 
-	switch c.ConfigType {
+	switch t.ConfigType {
 	case "toml", "tml":
 		tree, err := toml.LoadReader(buf)
 		if err != nil {
-			c.ConfigErr = err
+			t.ConfigErr = err
 			return
 		}
-		c.ParseResult = tree.ToMap()
+		t.ParseResult = tree.ToMap()
 	case "yaml", "yml":
-		if err := yaml.Unmarshal(buf.Bytes(), &c.ParseResult); err != nil {
-			c.ConfigErr = err
+		if err := yaml.Unmarshal(buf.Bytes(), &t.ParseResult); err != nil {
+			t.ConfigErr = err
 			return
 		}
+	default:
+		t.ConfigErr = errors.New("not supported config format")
+		return
 	}
 }
